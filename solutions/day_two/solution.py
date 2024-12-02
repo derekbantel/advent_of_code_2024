adae178febebf31c2613ccb8d14ed478556b7b80
derekbantel <derekbantel@dbantel.com>
2024-12-02 00:50:51 -0600

    Day two complete first part.

--- solutions/day_two/solution.py
+++ solutions/day_two/solution.py
@@ -1,6 +1,6 @@
 def read_data_file():
     list_of_items = []
-    with open("solutions/day_two/small.txt") as f:
+    with open("solutions/day_two/data.txt") as f:
         for line in f:
             list_of_items.append(line.strip().lower())
     list_of_items = [item for item in list_of_items if item != ""]
@@ -8,23 +8,32 @@ def read_data_file():
 
 
 def check_if_safe(line):
-    for idx, item in enumerate(line):
-        curr_num = int(item)
-        try:
-            next_num = int(line[idx + 1])
-            diff = abs(next_num - curr_num)
-            if 1 < diff <= 3:
-                return False
-        except IndexError:
-            continue
+    levels = list(map(int, line.split()))
+    if levels[0] < levels[1]:
+        line_status = "increased"
+    elif levels[0] > levels[1]:
+        line_status = "decreased"
+    else:
+        return False
+    for i in range(len(levels) - 1):
+        curr_num = levels[i]
+        next_num = levels[i + 1]
+        diff = abs(curr_num - next_num)
+        if not (1 <= diff <= 3):
+            return False
+        if line_status == "increased" and curr_num >= next_num:
+            return False
+        if line_status == "decreased" and curr_num <= next_num:
+            return False
     return True
+
 
 
 def day_two():
     safe_reports = 0
     file_data = read_data_file()
-    for line in file_data:
-        check_result = check_if_safe(line.split())
+    for idx, line in enumerate(file_data):
+        check_result = check_if_safe(line)
         if check_result:
             safe_reports += 1
     print(f"number of safe reports: {safe_reports}")
